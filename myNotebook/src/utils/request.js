38cf153a86878c241c72f1e6acac25dab0ac3867
qinpingwei <qinpingwei@rhsmed.com>
2026-06-18 17:00:21 +0800

    隐藏敏感信息

--- myNotebook/src/utils/request.js
+++ myNotebook/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getToken, removeToken } from './auth'
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
